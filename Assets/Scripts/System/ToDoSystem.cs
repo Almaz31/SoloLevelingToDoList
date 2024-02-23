aaf261c53c01b6d60f47c72425c84b7c5e5e3598
Almaz31 <58525531+Almaz31@users.noreply.github.com>
2024-02-23 20:18:34 +0200

    Edit Page Enchance

--- Assets/Scripts/System/ToDoSystem.cs
+++ Assets/Scripts/System/ToDoSystem.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class ToDoSystem : MonoBehaviour
 {
+    public ToDoSystem Instance { get; private set; }
+
+    private List<TaskItem> TaskItems;
+
+    private void Awake()
+    {
+        Instance = this; 
+    }
+    private void Start()
+    {
+        TaskItems=LoadTasks();
+    }
     public void SaveTasks(List<TaskItem> tasks)
     {
         string jsonTasks = JsonUtility.ToJson(tasks);
         PlayerPrefs.SetString("Tasks", jsonTasks);
         PlayerPrefs.Save();
     }
-
-    public List<TaskItem> LoadTasks()
+    public List<TaskItem> GetTasks()
+    {
+        return TaskItems;
+    }
+    private List<TaskItem> LoadTasks()
     {
         string jsonTasks = PlayerPrefs.GetString("Tasks");
         if (!string.IsNullOrEmpty(jsonTasks))
